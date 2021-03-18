80f28f7caf16447667caa25ad576cac37d10ab7a
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-18 22:22:40 +0300

    Create Constants.kt

---
+++ News/app/src/main/java/com/example/news/Util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.news.Util
+
+class Constants {
+    companion object{
+        const val API_KEY="90f78b5e459f4557a6d285161db89387"
+        const val BASE_URL="https://newsapi.org/v2/"
+
+    }
+}
