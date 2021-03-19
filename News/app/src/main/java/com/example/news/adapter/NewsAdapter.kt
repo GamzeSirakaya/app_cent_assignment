11ef218b2cc637c6fa4359c3c78e5e534142ae06
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-20 01:45:12 +0300

    Update NewsAdapter.kt

--- News/app/src/main/java/com/example/news/adapter/NewsAdapter.kt
+++ News/app/src/main/java/com/example/news/adapter/NewsAdapter.kt
@@ -1,15 +1,23 @@
 package com.example.news.adapter
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.news.R
+import com.example.news.util.ItemClickListener
 import com.example.news.databinding.RecyclerItemBinding
 import com.example.news.model.Article
+import com.example.news.view.HomeFragment
+import com.example.news.view.NewsDetailFragment
 
 
-class NewsAdapter(val newList: ArrayList<Article>) :
-    RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter :
+    RecyclerView.Adapter<NewsAdapter.NewsViewHolder>(),ItemClickListener {
 
 
     class NewsViewHolder(var view: RecyclerItemBinding) : RecyclerView.ViewHolder(view.root) {
@@ -28,35 +36,41 @@ class NewsAdapter(val newList: ArrayList<Article>) :
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        holder.view.articles = newList[position]
+        holder.view.articles = differ.currentList[position]
 
     }
 
     override fun getItemCount(): Int {
-        return newList.size
+        return differ.currentList.size
     }
 
-    fun newListUpdate(news: List<Article>) {
-        newList.clear()
-        newList.addAll(news)
+   /* fun newListUpdate(news: List<Article>) {
+        differ.currentList.clear()
+        differ.currentList.addAll(news)
         notifyDataSetChanged()
 
-    }
-
-}
-   /* companion object {
-        private val diffUtilCallBack =
-            object  : DiffUtil.ItemCallback<Article>(){
-                override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
-                    return oldItem.url == newItem.url
-                }
-                override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
-                    return oldItem==newItem
-                }
-
-            }
-
     }*/
+
+    override fun itemClickListener(view: View) {
+
+    Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_newsDetailFragment)
+    }
+     val differ= AsyncListDiffer(this, diffUtilCallBack)
+     companion object {
+         private val diffUtilCallBack =
+             object  : DiffUtil.ItemCallback<Article>(){
+                 override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+                     return oldItem.url == newItem.url
+                 }
+                 override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+                     return oldItem==newItem
+                 }
+
+             }
+
+     }
+}
+
 /*private var OnItemClickListener:((Article)->Unit)?=null
     fun setOnItemClickListener(listener:(Article)->Unit){
         OnItemClickListener=listener
