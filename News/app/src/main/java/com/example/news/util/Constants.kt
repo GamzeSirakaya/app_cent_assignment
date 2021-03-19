058c846d611463d482c7d218b88f96f746dffaae
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-20 01:47:47 +0300

    new

---
+++ News/app/src/main/java/com/example/news/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.news.util
+
+class Constants {
+    companion object{
+        const val API_KEY="90f78b5e459f4557a6d285161db89387"
+        const val BASE_URL="https://newsapi.org/v2/"
+
+
+    }
+}
---
+++ News/app/src/main/java/com/example/news/viewModel/NewsDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.news.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class NewsDetailViewModel:ViewModel() {
+
+}
