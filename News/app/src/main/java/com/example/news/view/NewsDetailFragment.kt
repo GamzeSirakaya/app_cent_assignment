62e91059d442681df82b500292f49b5095dae8a6
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-23 23:38:26 +0300

    Update NewsDetailFragment.kt

--- News/app/src/main/java/com/example/news/view/NewsDetailFragment.kt
+++ News/app/src/main/java/com/example/news/view/NewsDetailFragment.kt
@@ -14,7 +14,6 @@ import kotlinx.android.synthetic.main.fragment_news_detail.*
 import kotlinx.android.synthetic.main.recycler_item.*
 
 
-
 class NewsDetailFragment : Fragment(R.layout.fragment_news_detail) {
 
     val args: NewsDetailFragmentArgs by navArgs()
@@ -23,14 +22,14 @@ class NewsDetailFragment : Fragment(R.layout.fragment_news_detail) {
         super.onViewCreated(view, savedInstanceState)
         val article = args.article
         detail_title.text = article.title
-       detail_publishedAt.text = article.publishedAt.dateFormat()
-        detail_source.text=article.source.name
+        detail_publishedAt.text = article.publishedAt.dateFormat()
+        detail_source.text = article.source.name
         detail_desc.text = article.description
-        detail_hour.text=article.publishedAt?.dateToTimeFormat()
-        Glide.with(this).load(article.urlToImage).into(backdrop)
-        web_button.setOnClickListener {
+        detail_hour.text = article.publishedAt?.dateToTimeFormat()
+        Glide.with(this).load(article.urlToImage).into(detail_urlToImage)
+        openWeb.setOnClickListener {
             val bundle = Bundle().apply {
-                putSerializable("article",article)
+                putSerializable("article", article)
 
             }
 
@@ -40,21 +39,19 @@ class NewsDetailFragment : Fragment(R.layout.fragment_news_detail) {
             )
         }
 
-        shareBtn.setOnClickListener {
+        shareButton.setOnClickListener {
             try {
                 val i = Intent(Intent.ACTION_SEND)
                 i.type = "text/plan"
                 i.putExtra(Intent.EXTRA_SUBJECT, args.article.title)
                 val body: String =
-                    args.article.title.toString() + "\n" + args.article.url + "\n" + "Haber Paylaşımı" + "\n"
+                    args.article.title.toString() + "\n" + args.article.url + "\n" + "News Share" + "\n"
                 i.putExtra(Intent.EXTRA_TEXT, body)
                 startActivity(Intent.createChooser(i, "Share with :"))
             } catch (e: Exception) {
 
             }
         }
-
-
 
 
     }
