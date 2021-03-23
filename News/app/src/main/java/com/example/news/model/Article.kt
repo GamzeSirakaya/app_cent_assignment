7c9d1cf18870a3985e5083a39c8101c0295a832a
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-23 23:38:14 +0300

    Update Article.kt

--- News/app/src/main/java/com/example/news/model/Article.kt
+++ News/app/src/main/java/com/example/news/model/Article.kt
@@ -13,19 +13,20 @@ import java.io.Serializable
 data class Article(
     @PrimaryKey(autoGenerate = true)
     val id: Int? = null,
-    @ColumnInfo(name = "author")
+
     val author: String?,
-    @ColumnInfo(name = "content")
+
     val content: String?,
-    @ColumnInfo(name = "description")
+
     val description: String?,
-    @ColumnInfo(name = "publishedAt")
+
     val publishedAt: String?,
-   val source: Source,
-    @ColumnInfo(name = "title")
+
+    val source: Source,
+
     val title: String?,
-    @ColumnInfo(name = "url")
+
     val url: String?,
-    @ColumnInfo(name = "urlToImage")
+
     val urlToImage: String?
 ) : Serializable
