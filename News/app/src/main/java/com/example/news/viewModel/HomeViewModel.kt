f8f34324f6e07010073fac3b16f129d3d5901b81
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-23 23:38:19 +0300

    Update HomeViewModel.kt

--- News/app/src/main/java/com/example/news/viewModel/HomeViewModel.kt
+++ News/app/src/main/java/com/example/news/viewModel/HomeViewModel.kt
@@ -1,13 +1,15 @@
 package com.example.news.viewModel
 
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.news.model.Article
 import com.example.news.model.NewResponse
+import com.example.news.util.Constants.Companion.API_KEY
+import com.example.news.util.Constants.Companion.PAGE
+import com.example.news.util.Constants.Companion.PAGESİZE
 import com.example.news.util.getCountry
-import com.example.news.util.getLanguage
+
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
@@ -19,14 +21,16 @@ class HomeViewModel : ViewModel() {
     val newsError = MutableLiveData<Boolean>()
     val newsLoading = MutableLiveData<Boolean>()
     private val disposable = CompositeDisposable()
+   // private val pageSize = 10
+   //private lateinit var sourceFactory: NewsDataSourceFactory
 
-    init {
-        news()
+    fun refreshData() {
+        news(getCountry().toString(), API_KEY, PAGE, PAGESİZE)
+
     }
-
-    fun news() {
+    fun news(country: String, apiKey: String,page:Int,pageSize:Int) {
         disposable.add(
-            com.example.news.network.News.getService().getNews(getCountry().toString())
+            com.example.news.network.News.getService().getNews(country,apiKey,pageSize,page)
                 .subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<NewResponse>() {
                     override fun onSuccess(t: NewResponse) {
@@ -41,6 +45,43 @@ class HomeViewModel : ViewModel() {
                     }
                 }
                 ))
+
+      /* sourceFactory = NewsDataSourceFactory(
+            disposable,
+            News.getService(),
+            country = country,
+            apiKey = apiKey
+        )
+
+        val config = PagedList.Config.Builder()
+            .setPageSize(pageSize)
+            .setEnablePlaceholders(false)
+            .build()
+
+        val eventPagedList = RxPagedListBuilder(sourceFactory, config)
+            .setFetchScheduler(Schedulers.io())
+            .buildObservable()
+            .cache()
+        newsLoading.value = true
+
+        disposable.add(eventPagedList.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread()).doOnSubscribe { newsLoading.value = true }
+            .subscribe({
+                if (it.isNotEmpty()) {
+                    newResponse.value=it
+                    newsError.value = false
+                    newsLoading.value = false
+                }
+            }, {
+
+                newsError.value = true
+                newsLoading.value = false
+
+            })
+        )*/
+
+
+
     }
 
 
