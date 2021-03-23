0894fa51d09c1fa810c09ee14f102bd3f6d7b74e
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-23 23:38:42 +0300

    Update MainActivity.kt

--- News/app/src/main/java/com/example/news/view/MainActivity.kt
+++ News/app/src/main/java/com/example/news/view/MainActivity.kt
@@ -2,20 +2,14 @@ package com.example.news.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.news.R
-import com.example.news.db.ArticleDatabase
-import com.example.news.repository.NewsRepository
-import com.example.news.viewModel.HomeViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewModel: HomeViewModel
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
